4380bdfbf4fc68f89d8f7d42fe9464c9edddd3fc
CasaRol <32219634+CasaRol@users.noreply.github.com>
2020-07-17 19:19:47 +0200

    Server can now receive CharArray for the gameboard
    Testing client created but not intended for release (must be deleted later)

--- TicTacToe-Server/src/tictactoe/server/TestClient.java
+++ TicTacToe-Server/src/tictactoe/server/TestClient.java
@@ -5,7 +5,12 @@
  */
 package tictactoe.server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Array;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,19 +21,52 @@ import java.util.logging.Logger;
  */
 public class TestClient {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private Socket client = null;
+    private BufferedReader input = null;
+    private ObjectOutputStream output = null;
+    private char[] game = null;
+
+    public TestClient(String address, int port) {
         System.out.println("Starting client...");
         try {
-            Socket client = new Socket("127.0.0.1", 8080);
+            game = new char[9];
+            client = new Socket("127.0.0.1", 8080);
+            
+            input = new BufferedReader(new InputStreamReader(System.in));
+            output = new ObjectOutputStream(client.getOutputStream());
+            System.out.println("Client is connected and ready. Please enter your message.");
+
         } catch (IOException ex) {
             Logger.getLogger(TestClient.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+            try {
+                for(int i = 0; i < game.length; i++) {
+                    if(i == 5 || i == 2) {
+                        continue;
+                    }
+                    game[i] = 'c';
+                }
+                output.writeObject(game);
+                System.out.println("output written");
+                output.flush();
+                System.out.println("Flushing");
+                client.close();
+            } catch (IOException IoE) {
+                System.out.println(IoE.toString());
+            }
         
-        System.out.println("Client should be connected(?)");
+
+        /*try {
+            input.close();
+            output.close();
+            client.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } */
     }
-    
+
+    public static void main(String[] args) {
+        TestClient testClient = new TestClient("127.0.0.1", 8080);
+    }
 }
--- TicTacToe-Server/src/tictactoe/server/TicTacToeServer.java
+++ TicTacToe-Server/src/tictactoe/server/TicTacToeServer.java
@@ -6,8 +6,12 @@
 package tictactoe.server;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,19 +19,56 @@ import java.net.Socket;
  */
 public class TicTacToeServer {
 
-    /**
-     * @param args the command line arguments
-     */
+    private ServerSocket server = null;
+    private Socket client = null;
+    private ObjectInputStream input = null;
+    private PrintWriter output = null;
+    private char[] game = null;
+
+    public TicTacToeServer(int port) {
+        try {
+            game = new char[9];
+            server = new ServerSocket(port);
+            System.out.println("Server is now listening on port " + port);
+            System.out.println("Awaiting client connection...");
+
+            client = server.accept();
+            System.out.println("Client has connected");
+
+            input = new ObjectInputStream(client.getInputStream());
+
+            while (input != null) {
+                try {
+                    char[] gameArray = (char[]) input.readObject();
+                    System.out.println("Array has been filled");
+                    for (int i = 0; i < gameArray.length; i++) {
+                        System.out.println("index " + i + "= " + gameArray[i]);
+                    }
+
+                    //remove after testing!
+                    System.exit(0);
+                } catch (IOException IOE) {
+                    System.out.println(IOE.toString());
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            try {
+                input.close();
+                client.close();
+            } catch (IOException IOE) {
+                System.out.println(IOE.toString());
+            }
+
+        } catch (IOException IOE) {
+            System.out.println(IOE.toString());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        ServerSocket serverSocket = new ServerSocket(8080);
-        
-        System.out.println("Server now listening on port 8080");
-        
-        System.out.println("Awaiting connection from client...");
-        
-        Socket socket = serverSocket.accept();
-        
+        TicTacToeServer server = new TicTacToeServer(8080);
     }
-    
+
 }
